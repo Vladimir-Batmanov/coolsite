5bb00127d06a4582950df7c93b4cc78766118450
Vladimir Batmanov <vladimirbatmanov@Vladimirs-MacBook-Pro.local>
2023-04-25 20:43:37 +0300

    Удалил базу данных и все миграции, чтобы создать новую, но уже с колонкой - slug. Добавил slug к URL-адресам катерогий и постов.

--- women/views.py
+++ women/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 
 # Create your views here.
@@ -35,12 +35,21 @@ def login(request):
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
-def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
 
-def show_category(request, cat_id):
-    posts = Women.objects.filter(cat_id=cat_id)
+    context = {
+        'post': post,
+        'menu': menu,
+        'title': post.title,
+        'cat_selected': post.cat_id,
+    }
 
+    return render(request, 'women/post.html', context=context)
+
+def show_category(request, cat_slug):
+    posts = Women.objects.filter(cat__slug=cat_slug)
+≠
     if len(posts) == 0:
         raise Http404()
 
@@ -48,6 +57,6 @@ def show_category(request, cat_id):
         'posts': posts,
         'menu': menu,
         'title': 'Отображение по рубрикам',
-        'cat_selected': cat_id,
+        'cat_selected': cat_slug,
     }
     return render(request, 'women/index.html', context=context)
