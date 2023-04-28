89ca1ac420482cc4c7676cef2049bbae14c0d599
Vladimir Batmanov <vladimirbatmanov@Vladimirs-MacBook-Pro.local>
2023-04-28 12:21:59 +0300

    Добавил постраничную навигацию (пагинация).

--- women/admin.py
+++ women/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
-
 from .models import *
 
 class WomenAdmin(admin.ModelAdmin):
@@ -10,13 +8,13 @@ class WomenAdmin(admin.ModelAdmin):
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create')
-    prepopulated_fields = {'slug': ("title",)}
+    prepopulated_fields = {"slug": ("title",)}
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     list_display_links = ('id', 'name')
     search_fields = ('name',)
-    prepopulated_fields = {'slug': ("name",)}
+    prepopulated_fields = {"slug": ("name",)}
 
 
 admin.site.register(Women, WomenAdmin)
--- women/forms.py
+++ women/forms.py
@@ -1,11 +1,12 @@
 from django import forms
-from .models import *
 from django.core.exceptions import ValidationError
+
+from .models import *
 
 class AddPostForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['cat'].empty_label = 'Категория не выбрана'
+        self.fields['cat'].empty_label = "Категория не выбрана"
 
     class Meta:
         model = Women
@@ -16,8 +17,8 @@ class AddPostForm(forms.ModelForm):
         }
 
     def clean_title(self):
-        title = self.changed_data['title']
+        title = self.cleaned_data['title']
         if len(title) > 200:
             raise ValidationError('Длина превышает 200 символов')
-        
+
         return title
