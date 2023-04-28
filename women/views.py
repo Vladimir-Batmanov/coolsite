a57007f6b1f5c89e670ed3de94b43f7ab5e2aa6b
Vladimir Batmanov <vladimirbatmanov@Vladimirs-MacBook-Pro.local>
2023-04-28 12:56:14 +0300

    Добавил регистрацию пользователей на сайте.

--- women/views.py
+++ women/views.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404
@@ -5,6 +6,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
 
 from .forms import *
 from .models import *
@@ -134,3 +136,12 @@ class WomenCategory(DataMixin, ListView):
 #
 #     return render(request, 'women/index.html', context=context)
 
+class RegisterUser(DataMixin, CreateView):
+    form_class = UserCreationForm
+    template_name = 'women/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) +list(c_def.items()))
